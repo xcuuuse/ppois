f10241aa03b62be47c4710e2ad1a93cbfa26e145
Alex Yevik <aleksejevik92@gmail.com>
2025-11-23 14:02:19 +0300

    added documentation

--- lab3/Restaurant/Employee/manager.py
+++ lab3/Restaurant/Employee/manager.py
@@ -5,10 +5,14 @@ class EmployeeManager:
     def __init__(self):
         self.__employees: dict[int, Employee] = {}
 
+    @property
+    def employees(self):
+        return self.__employees
+
     def add_employee(self, new_employee: Employee):
         self.__employees[new_employee.employee_id] = new_employee
 
-    def adjust_salary(self, employee_id: int, amount: float):
+    def adjust_employee_salary(self, employee_id: int, amount: float):
         employee = self.__employees.get(employee_id)
         employee.increase_salary(amount)
 
@@ -16,3 +20,7 @@ class EmployeeManager:
         employee = self.__employees.get(employee_id)
         employee.increase_salary(-amount)
 
+    def fire_employee(self, employee_id: int):
+        del self.__employees[employee_id]
+
+    
